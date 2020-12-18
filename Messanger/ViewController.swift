62f100226d4e9d64f2239053b7a90ef9870dd88e
Ebube-sudo <emac5986@gmail.com>
2020-12-19 00:46:42 +0100

    Installed Firebase pods

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Ebube the Programmer on 16/12/2020.
+//  Copyright © 2020 Ebube the Programmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
